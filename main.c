4ec95e93e52f802013db7376814f2fbf468bd18f
CHOI TAEYOUNG <55708666+92-Theo@users.noreply.github.com>
2024-01-22 15:03:38 +0900

    update test code (AES-CCM)

--- main.c
+++ main.c
@@ -70,9 +70,11 @@
  *
  */
 
-#define AES_MAC_SIZE                            (16)
+#define AES_MAC_SIZE                                      (16)
+#define TEST_AES_MAC_SIZE                                 (10)
 
-#define NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE    (100)
+#define NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE              (100)
+#define TEST_NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE         (100)
 
 #define AES_ERROR_CHECK(error)  \
     do {            \
@@ -92,14 +94,26 @@ static uint8_t m_key[32] = {'N', 'O', 'R', 'D', 'I', 'C', ' ',
                             'S', 'E', 'M', 'I', 'C', 'O', 'N', 'D', 'U', 'C', 'T', 'O', 'R',
                             'A', 'E', 'S', '&', 'M', 'A', 'C', ' ', 'T', 'E', 'S', 'T'};
 
+static uint8_t m_test_key[16] = {'t', 'e', 's', 't', '1', '2',
+                            't', 'e', 's', 't', '1', '2',
+                            '1', '2', '3', '4'};
+
 /* Below text is used as plain text for encryption, decryption and MAC calculation. */
 static char m_plain_text[NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE] =
 {
     "Example string used to demonstrate basic usage of AES CCM mode."
 };
 
+static char m_test_plain_text[] = {                  // 16 bytes
+    0x44, 0x44, 0x44, 0x30, 0x30, 0x30, 
+    0x30, 0x30, 0x30, 0x72, 0xFB, 0xBA, 
+    0x10, 0x19, 0xEF, 0xD9
+};
+
 static char m_encrypted_text[NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE];
 static char m_decrypted_text[NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE];
+static char m_test_encrypted_text[TEST_NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE];
+static char m_test_decrypted_text[TEST_NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE];
 
 static void text_print(char const* p_label, char const * p_text, size_t len)
 {
@@ -143,6 +157,16 @@ static void plain_text_print(void)
 {
     text_print("Plain text", m_plain_text, strlen(m_plain_text));
     hex_text_print("Plain text (hex)", m_plain_text, strlen(m_plain_text));
+}
+
+static void test_text_print(char const * p_text, size_t len)
+{
+    hex_text_print("Text (hex)", p_text, len);
+}
+
+static void test_plain_text_print(char const * p_text, size_t len)
+{
+    hex_text_print("Plain text (hex)", p_text, len);
 }
 
 static void encrypted_text_print(char const * p_text, size_t encrypted_len)
@@ -231,6 +255,79 @@ static void crypt_ccm(void)
     }
 }
 
+static void test_crypt_ccm(void)
+{
+    uint32_t    len;
+    ret_code_t  ret_val;
+
+    static uint8_t     mac[TEST_AES_MAC_SIZE];   // 10bytes
+    static uint8_t     nonce[]  = {                 // 12 bytes
+      0x6B, 0x65, 0x79, 0x70, 0x6C, 0x65,
+      0x09, 0x67, 0xC5, 0x69, 0x44, 0xDB
+    };
+    
+    static nrf_crypto_aead_context_t ccm_ctx;
+
+    memset(mac,   0, sizeof(mac));
+    // memset(nonce, 0, sizeof(nonce));
+
+    
+    len = sizeof(m_test_plain_text);
+
+    test_plain_text_print(m_test_plain_text, len);
+
+    /* Init encrypt and decrypt context */
+    ret_val = nrf_crypto_aead_init(&ccm_ctx,
+                                   &g_nrf_crypto_aes_ccm_128_info,
+                                   m_test_key);
+    AES_ERROR_CHECK(ret_val);
+
+    /* encrypt and tag text */
+    ret_val = nrf_crypto_aead_crypt(&ccm_ctx,
+                                    NRF_CRYPTO_ENCRYPT,
+                                    nonce,
+                                    sizeof(nonce),
+                                    NULL,
+                                    0,
+                                    (uint8_t *)m_test_plain_text,
+                                    len,
+                                    (uint8_t *)m_test_encrypted_text,
+                                    mac,
+                                    sizeof(mac));
+    AES_ERROR_CHECK(ret_val);
+
+    encrypted_text_print(m_test_encrypted_text, len);
+    mac_print(mac, sizeof(mac));
+
+    /* decrypt text */
+    ret_val = nrf_crypto_aead_crypt(&ccm_ctx,
+                                    NRF_CRYPTO_DECRYPT,
+                                    nonce,
+                                    sizeof(nonce),
+                                    NULL,
+                                    0,
+                                    (uint8_t *)m_test_encrypted_text,
+                                    len,
+                                    (uint8_t *)m_test_decrypted_text,
+                                    mac,
+                                    sizeof(mac));
+    AES_ERROR_CHECK(ret_val);
+
+    ret_val = nrf_crypto_aead_uninit(&ccm_ctx);
+    AES_ERROR_CHECK(ret_val);
+
+    decrypted_text_print(m_test_decrypted_text, len);
+
+    if (memcmp(m_test_plain_text, m_test_decrypted_text, strlen(m_test_plain_text)) == 0)
+    {
+        NRF_LOG_RAW_INFO("AES CCM example executed successfully.\r\n");
+    }
+    else
+    {
+        NRF_LOG_RAW_INFO("AES CCM example failed!!!.\r\n");
+    }
+}
+
 int main(void)
 {
     ret_code_t ret;
@@ -251,7 +348,8 @@ int main(void)
     ret = nrf_mem_init();
     APP_ERROR_CHECK(ret);
 
-    crypt_ccm();
+    // crypt_ccm();
+    test_crypt_ccm();
     while (true)
     {
         NRF_LOG_FLUSH();
